26ca29a1401fa50b5d8d58d511e935f10a4cf641
sasimapa <sdilshara26@gmail.com>
2023-09-02 14:23:09 +0530

    create navigation menu before authentication

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+
 function NavBar() {
-  return <div>NavBar</div>;
+  let isUserLoggedIn = true;
+
+  return (
+    <nav>
+      <div className="nav-left">
+        <Image src="/logo.png" alt="logo" width={30} height={30} />
+      </div>
+      <div className="nav-title">NEXT BLOG</div>
+      <div className="nav-right">
+        {isUserLoggedIn ? (
+          <div>
+            <button className="nav-btn">Create Post</button>
+            <Image src="/logo.png" alt="profile" width={30} height={30} />
+          </div>
+        ) : (
+          <div>
+            <button className="nav-btn">Sign In</button>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
